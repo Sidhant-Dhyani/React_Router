ac64ec3b1a358c91abd9617c2e8d5f1246157db5
sidhant dhyani <siddhantydhyani99@gmail.com>
2023-12-26 17:14:31 +0530

    Error boundary added

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 
 const User = () => {
@@ -8,4 +8,4 @@ const User = () => {
     )
 }
 
-export default User
+export default User;
